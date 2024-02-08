277a9f6a8459ebffe5d00368f2097ee5a179ee21
Suraj Jha <suraj.jha@cnhind.com>
2024-02-08 19:24:16 +0530

    Made a .primal extension and added scenes & projects related meta-data and extensions inside that that are serialized XML

--- PrimalEditor/Common/ViewModelBase.cs
+++ PrimalEditor/Common/ViewModelBase.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.Runtime.Serialization;
 
 namespace PrimalEditor
 {
+    [DataContract(IsReference = true)]
     public class ViewModelBase:INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
--- PrimalEditor/obj/Debug/PrimalEditor.AssemblyInfo.cs
+++ PrimalEditor/obj/Debug/PrimalEditor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimalEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fde9d4a8a2b402a9c908b2be1f24f0916c9ee8d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimalEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimalEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
