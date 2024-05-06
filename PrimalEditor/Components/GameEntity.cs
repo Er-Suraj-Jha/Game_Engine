4d9f1657c64a02557d28dbba7f88ba3d31fa2090
Suraj Jha <suraj.jha@cnhind.com>
2024-05-06 14:41:43 +0530

    Added the tests,entity support dlls and invoked it from the C#

--- PrimalEditor/Components/GameEntity.cs
+++ PrimalEditor/Components/GameEntity.cs
@@ -6,8 +6,10 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Runtime.Serialization;
 using System.Text;
+using System.Threading.Channels;
 using System.Windows.Input;
 using System.Xml.Linq;
+using PrimalEditor.DllWrappers;
 
 namespace PrimalEditor.Components
 {
@@ -15,6 +17,42 @@ namespace PrimalEditor.Components
     [KnownType(typeof(Transform))]
     class GameEntity : ViewModelBase
     {
+        private int _entityId = ID.INVALID_ID;
+        public int EntityId
+        {
+            get => _entityId;
+            set
+            {
+                if (_entityId != value)
+                {
+                    _entityId = value;
+                    OnPropertyChanged(nameof(EntityId));
+                }
+            }
+        }
+
+        private bool _isActive;
+        public bool IsActive
+        {
+            get => _isActive;
+            set
+            {
+                if (_isActive != value)
+                {
+                    _isActive = value;
+                    if (_isActive)
+                    {
+                        EntityId = EngineAPI.CreateGameEntity(this);
+                        Debug.Assert(ID.IsValid(_entityId));
+                    }
+                    else
+                    {
+                        EngineAPI.RemoveGameEntity(this);
+                    }
+                    OnPropertyChanged(nameof(IsActive));
+                }
+            }
+        }
 
         private bool _isEnabled = true;
 
@@ -53,6 +91,10 @@ namespace PrimalEditor.Components
         [DataMember(Name = nameof(Components))]
         private readonly ObservableCollection<Component> _components = new ObservableCollection<Component>();
         public ReadOnlyObservableCollection<Component> Components { get; private set; }
+
+        public Component GetComponent(Type type) => Components.FirstOrDefault(c => c.GetType() == type);
+
+        public T GetComponent<T>() where T : Component => GetComponent(typeof(T)) as T;
 
         [OnDeserialized]
         void OnDeserialized(StreamingContext context)
